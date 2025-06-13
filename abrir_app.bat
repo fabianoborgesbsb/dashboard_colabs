519317644e44ae675f9ec010f5900bdd07f6cb29
Fabiano Borges <fabianoborgesbsb@hotmail.com>
2025-06-13 08:55:58 -0300

    Update abrir_app.bat

--- abrir_app.bat
+++ abrir_app.bat
@@ -1,4 +1,4 @@
 @echo off
-cd /d "C:\Fabiano Borges 2019\Meus estudos cientometria  2021\Arquivos Python\tabelaconsolidada_2024_400mil\bienio_QGIS_dashboard\4 STI cognitive"
+cd /d "file path"
 "C:\Users\User\anaconda3\python.exe" -m streamlit run app.py
 pause
